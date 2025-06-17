5d783aad78637903ba5681646f009c914c30b20a
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2025-06-17 18:39:46 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,12 +1,9 @@
 from hate.logger import logging
 from hate.exception import CustomException
 import sys
+from hate.configuration.gcloud_syncer import GCloudSync
 
-#logging.info("Welcome to our project")
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hate-speech008","dataset.zip","dataset.zip")
 
-try:
-    a = 7 / 0
-    
-except Exception as e:
-    raise CustomException(e,sys)
 
