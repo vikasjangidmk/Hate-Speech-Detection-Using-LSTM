c9a18f14c95afbbc66d0d4540f6d8d1c18c6d5d2
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2025-06-18 18:03:13 +0530

    Data Transformers

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ from hate.pipeline.train_pipeline import TrainPipeline
 obj = TrainPipeline()
 obj.run_pipeline()
 
+
--- hate/entity/artifact_entity.py
+++ hate/entity/artifact_entity.py
@@ -6,4 +6,8 @@ class DataIngestionArtifacts:
     imbalance_data_file_path: str
     raw_data_file_path: str
 
+@dataclass
+class DataTransformationArtifacts:
+    transformed_data_path: str
+
 
